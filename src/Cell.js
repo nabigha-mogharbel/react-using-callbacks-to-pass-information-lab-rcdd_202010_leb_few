52765aa954f19a13587168f6bf8e166cea9193db
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-27 21:24:11 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -8,11 +8,9 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
-   handleClick = () => {
-    this.setState({
+   handleClick = () => {this.setState({
       color: this.props.selectedColor
-    })
-  }
+    })}
   
   render() {
     return (
